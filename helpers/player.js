a6eff0af7e6167dcd89d9713cca175c08c045e91
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-11-18 18:34:52 +0800

    feat: animate 1 sprite horizontally

--- helpers/player.js
+++ helpers/player.js
@@ -1,18 +1,30 @@
+import { Sitting, Running, Jumping, Falling } from './playerStates.js'
+
 export class Player {
     constructor(game){
         this.game = game;
-        this.width = 107; // single frame width // robber-run=98 w 43 frames // box-run=107
-        this.height = 170; // single frame height // robber-run=139 // box-run=170 w 21 frames
+        this.width = 107; // single frame width
+        this.height = 170; // single frame height
         this.x = 50;
         this.y = this.game.height - this.height;
         this.vy = 0;
         this.weight = 1;
         // javascript automatically creates references to all elements with IDs into the global namespace, using it's ID as a variable name:
-        this.image = document.getElementById('player'); 
+        this.image = player; // this.image = document.getElementById('player'); 
+        this.frameX = 0;
+        this.frameY = 0;
+        this.maxFrame = 5;
+        this.fps = 60;
+        this.frameInterval = 1000/this.fps;
+        this.frameTimer = 0;
         this.speed = 0;
         this.maxSpeed = 10;
+        this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)];
+        this.currentState = this.states[0];
+        this.currentState.enter();
     }
-    update(input){
+    update(input, deltaTime){
+        this.currentState.handleInput(input);
         // horizontal movement
         this.x += this.speed;
         if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
@@ -21,19 +33,30 @@ export class Player {
         if (this.x < 0) this.x = 0;
         if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
         // vertical movement
-        if (input.includes('ArrowUp') && this.onGround()) this.vy -= 28;
         this.y += this.vy;
         if (!this.onGround()) this.vy += this.weight;
         else this.vy = 0;
+        // sprite animation
+        if (this.frameTimer > this.frameInterval){
+            this.frameTimer = 0;
+            if (this.frameX < this.maxFrame) this.frameX++;
+            else this.frameX = 0;
+        } else {
+            this.frameTimer += deltaTime;
+        }
     }
     draw(context){
         // visual aid
         context.strokeStyle = 'red';
         context.strokeRect(this.x, this.y, this.width, this.height);
         // draw
-        context.drawImage(this.image, this.width*0,this.height*0,this.width,this.height, this.x,this.y,this.width,this.height);
+        context.drawImage(this.image, this.width*this.frameX,this.height*this.frameY,this.width,this.height, this.x,this.y,this.width,this.height);
     }
     onGround(){
         return this.y >= this.game.height - this.height;
+    }
+    setState(state){
+        this.currentState = this.states[state];
+        this.currentState.enter();
     }
 }
